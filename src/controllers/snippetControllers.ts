7bf78b8cd47078afe36189564bf661f65afcc417
unknown <hemantatwork>
2024-06-12 11:05:55 +0530

    api improved and consoles removed

--- src/controllers/snippetControllers.ts
+++ src/controllers/snippetControllers.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { Snippet } from "../db/model";
 
 export async function createSnippet(req: Request, res: Response) {
-  console.log(" req.body => ", req.body);
   const snippet = new Snippet({
     snippet: req.body.snippet,
     snippet_type: req.body.snippet_type,
@@ -10,23 +9,38 @@ export async function createSnippet(req: Request, res: Response) {
   });
   try {
     const data = await snippet.save();
-    console.log("data => ", data);
-    res.send(data);
+    res.send({
+      success: true,
+      data: data,
+      message: "Snippet created successfully",
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Something went wrong while saving snippet" });
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong while saving snippet",
+    });
   }
 }
 
 export async function getSnippet(req: Request, res: Response) {
   try {
     const data = await Snippet.findById(req.params.key);
-    res.json(data);
+    if (data === null) {
+      return res.json({
+        success: true,
+        message: "No snippet found.",
+      });
+    }
+    res.json({
+      success: true,
+      data: data,
+      message: "Snippet fetched successfully",
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Something went wrong with fetching snippet" });
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong while fetching snippet",
+    });
   }
 }
 
@@ -35,16 +49,19 @@ export async function updateSnippet(req: Request, res: Response) {
     const id = req.params.key;
     const updatedData = req.body;
     const options = { new: true };
-    console.log("id => ", id);
-    console.log("updatedData => ", updatedData);
 
     const result = await Snippet.findByIdAndUpdate(id, updatedData, options);
-    console.log("result", result);
-    res.send(result);
+
+    res.send({
+      success: true,
+      data: result,
+      message: "snippet updated successfully",
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Something went wrong with update snippet" });
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong with update snippet",
+    });
   }
 }
 
@@ -53,12 +70,19 @@ export async function deleteSnippet(req: Request, res: Response) {
     const id = req.params.key;
     const data = await Snippet.findByIdAndDelete(id);
     if (!data) {
-      res.send(`Snippet not found to delete or has been already deleted.`);
+      res.send({
+        success: true,
+        message: `Snippet not found to delete or has been already deleted`,
+      });
     }
-    res.send(`Snippet with ${data?._id} has been deleted..`);
+    res.send({
+      success: true,
+      message: `Snippet with ${data?._id} has been deleted..`,
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Something went wrong with delete snippet" });
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong with delete snippet",
+    });
   }
 }
